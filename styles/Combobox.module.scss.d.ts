80aea8cc460661980200b1953be0982764c842e9
Naoto92X82V99 <naoto5018@gmail.com>
2021-08-25 20:36:58 +0900

    feat: add combobox

---
+++ styles/Combobox.module.scss.d.ts
@@ -0,0 +1,12 @@
+export type Styles = {
+  'combobox': string;
+  'icon': string;
+  'input-container': string;
+  'list': string;
+};
+
+export type ClassNames = keyof Styles;
+
+declare const styles: Styles;
+
+export default styles;
